d1e885f09c522229972c03cf591644a15180c7f7
Amanda Wright <148620370+AWri13@users.noreply.github.com>
2024-04-10 12:30:18 -0400

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -53,7 +53,7 @@ app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-let PORT = process.env.PORT || 3000;
+let port = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Helmet ${PORT}`);
 });
